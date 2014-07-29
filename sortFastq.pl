0f1e43759dd6197430d538fda028984c8a56b9a7
Lee Katz - Aspen <gzu2@cdc.gov>
2014-07-29 12:03:31 -0400

    sorting by reads' ATGC alphabetical or GC content

--- sortFastq.pl
+++ sortFastq.pl
@@ -11,16 +11,33 @@ sub logmsg{print STDERR "@_\n";}
 exit main();
 sub main{
   my $settings={};
-  GetOptions($settings,qw(help reference=s tempdir=s numcpus=i paired)) or die;
+  GetOptions($settings,qw(help reference=s tempdir=s numcpus=i paired method=s)) or die;
   $$settings{tempdir}||=mktempdir();
   $$settings{numcpus}||=1;
   $$settings{paired}||=0;
+  $$settings{reference}||="";
+  $$settings{method}||="map";
+  $$settings{method}=lc($$settings{method});
   
   die usage() if($$settings{help});
 
   my ($fastq1,$fastq2)=writeFastq($settings);
 
-  my $reference=$$settings{reference};
+  my ($reads1,$reads2)=({},{}); # hashes to convert to fastq, to write to stdout
+  if($$settings{method} eq 'map'){
+    ($reads1,$reads2)=sortByMapping($$settings{reference},$fastq1,$fastq2,$settings);
+  } elsif($$settings{method} eq 'binbygc' || $$settings{method} eq 'binbyalpha'){
+    ($reads1,$reads2)=sortByBinning($fastq1,$fastq2,$settings);
+  } else{
+    die "Internal error: could not figure out what kind of sorting you wanted (user specified '$$settings{method}')";
+  }
+
+
+  return 0;
+}
+
+sub sortByMapping{
+  my($reference,$fastq1,$fastq2,$settings)=@_;
   if(!$reference){
     ...;
     logmsg "A reference was not specified. Assembling the reads.";
@@ -31,10 +48,8 @@ sub main{
   my $bam=mapReads($reference,$fastq1,$fastq2,$settings);
   my $reads1=readFastq($fastq1,$settings);
   my $reads2=readFastq($fastq2,$settings);
-  my $numReads=printReads($bam,$reads1,$reads2,$settings);
+  my $numReads=printReadsFromBam($bam,$reads1,$reads2,$settings);
   logmsg "$numReads reads or pairs printed to stdout";
-
-  return 0;
 }
 
 sub writeFastq{
@@ -99,7 +114,7 @@ sub mktempdir(;$) {
   return $tempdir;
 }
 
-sub printReads{
+sub printReadsFromBam{
   my($bam,$reads1,$reads2,$settings)=@_;
   my $i=0;
   if($$settings{paired}){
@@ -123,6 +138,31 @@ sub printReads{
   return $i;
 }
 
+sub sortByBinning{
+  my($fastq1,$fastq2,$settings)=@_;
+  my $unsorted1=readFastqIntoHash($fastq1,$settings);
+  my $unsorted2=readFastqIntoHash($fastq2,$settings);
+
+  my @sorted1;
+  if($$settings{method} eq 'binbyalpha'){
+    @sorted1=sort{$$a{seq} cmp $$b{seq} } values(%$unsorted1);
+  } elsif ($$settings{method} eq 'binbygc'){
+    @sorted1=sort{$$a{gc} <=> $$b{gc}} values(%$unsorted1);
+  } else {
+    die "ERROR: I could not understand method ($$settings{method})";
+  }
+
+  # print out the results
+  my $numReads=@sorted1;
+  for(my $i=0;$i<$numReads;$i++){
+    print "$sorted1[$i]{defline}\n$sorted1[$i]{seq}\n+\n$sorted1[$i]{qual}\n";
+    if($$settings{paired}){
+      my $key=$sorted1[$i]{key};
+      print "$$unsorted2{$key}{defline}\n$$unsorted2{$key}{seq}\n+\n$$unsorted2{$key}{qual}\n" if($$settings{paired});
+    }
+  }
+}
+
 sub readFastq{
   my($file,$settings)=@_;
   logmsg "Reading $file into memory";
@@ -137,6 +177,30 @@ sub readFastq{
     <FASTQ>; # burn the + line
     $$seqs{$key}.="+\n"; # but add in my own
     $$seqs{$key}.=<FASTQ>; # qual
+  }
+  close FASTQ;
+  return $seqs;
+}
+sub readFastqIntoHash{
+  my($file,$settings)=@_;
+  logmsg "Reading $file into memory";
+  my $seqs;
+  open(FASTQ,$file) or die "ERROR: cannot read $file: $!";
+  while(my $defline=<FASTQ>){
+    my ($key,@desc)=split(/\s+/,$defline);
+    die "ERROR: no key for $defline" if(!$key);
+    $key=~s/^\@//;
+    $$seqs{$key}{defline}=$defline; # whole defline
+    $$seqs{$key}{key}=$key;         # text before whitespace
+    $$seqs{$key}{desc}=join(" ",@desc);
+    $$seqs{$key}{seq}=<FASTQ>;
+    <FASTQ>; # burn the + line
+    $$seqs{$key}{qual}=<FASTQ>;
+    chomp($$seqs{$key}{qual},$$seqs{$key}{defline},$$seqs{$key}{seq},$$seqs{$key}{desc});
+
+    # some derivative values that will need to be calculated
+    $$seqs{$key}{length}=length($$seqs{$key}{seq});
+    $$seqs{$key}{gc}=($$seqs{$key}{seq}=~s/([GC])/$1/gi)/$$seqs{$key}{length};
   }
   close FASTQ;
   return $seqs;
@@ -162,6 +226,10 @@ sub usage{
   "Allows for better compression of reads by sorting them. Uses smalt under the hood. CGP fast assembly for an assembler.
   Usage: $0 [-r reference.fasta] < reads.fastq | gzip -c > sorted.fastq.gz
   -r reference.fasta If not supplied, then the reads will be assembled to create one.
+  -m methodology  Values: map, binByAlpha, binByGC  Default: map
+    map: map reads onto a reference genome given by -r
+    binByAlpha: alphabetize the reads
+    binByGC: bin the reads according to their nucleotide composition
   -t tempdir/ Default: one will be created for you
   --numcpus How many cpus to use when mapping. Default: 1
   -p to describe paired-end
