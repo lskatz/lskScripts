9bebc61d4c0d02ee4636ae1ec47f63ef97ac87c7
Lee - spacestation4.cdc.gov <gzu2@cdc.gov>
2016-02-08 09:28:07 -0500

    init; not yet working

---
+++ Kendall.R
@@ -0,0 +1,51 @@
+#!/bin/env Rscript
+
+# Libraries
+library(stringr)
+library(treescape)
+library(phangorn)
+library(getopt)
+
+# Import data
+# Column  3: Argument mask  of  the flag , an integer
+# Possible  values:  0=no argument, 1=required argument, 2=optional argument.
+spec = matrix(c(
+  'help'   , 'h', 0, "logical",   'A call for help',
+  'tree1'  , '1', 1, "character", 'The first tree',
+  'tree2'  , '2', 1, "character", 'The second tree'
+), byrow=TRUE, ncol=5);
+opt = getopt(spec);
+#stop("STOP")
+
+# Concatenate the trees and then lead them
+stop("STOP")
+
+
+## Get a list of all Newick files in the directory
+treefiles <- list.files(path = "LeeTesting", 
+                        all.files = FALSE,
+                        full.names = FALSE)
+ntrees <- length(treefiles)
+
+## Loop over files and import as multiphylo object
+mytrees <- vector("list", ntrees)
+class(mytrees) <- "multiphylo"
+for(f in 1:ntrees) {
+  ### This is actually reading the tree file, doing a midpoint root, 
+  ### and storing it as a list in one slot of the vector.
+  mytrees[f] <- list(midpoint(read.tree(file = paste("LeeTesting/", treefiles[f], sep = ""))))
+}
+
+## Cleaning up the tip labels on the second tree because they don't match the first. 
+## Idiosyncratic.
+for(t in 1:length(mytrees[[2]]$tip.label)) {
+  mytrees[[2]]$tip.label[t] <- str_split(string = mytrees[[2]]$tip.label[t], pattern = "_")[[1]][1]
+}
+mytrees <- .compressTipLabel(mytrees)
+
+# Calculating the Kendall pairwise distance
+lambdas <- c(0, 0.5, 0.7, 1.0)
+for(x in 1:4) {
+  print(paste("lambda = ", lambdas[x]))
+  print(multiDist(mytrees, lambda = lambdas[x]))
+}
