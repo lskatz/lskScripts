b03a5b3bb3ce7d759ccdacd1bce258747c6da8df
Lee Katz - Aspen <gzu2@cdc.gov>
2016-05-02 10:19:54 -0400

    new options

--- treeInfo.pl
+++ treeInfo.pl
@@ -11,11 +11,20 @@ exit(main());
 
 sub main{
   my $settings={};
-  GetOptions($settings,qw(help)) or die $!;
+  GetOptions($settings,qw(help taxa confidence|bootstrap all cutoff=i)) or die $!;
+  $$settings{cutoff}||=0;
+
+  # If the user wants all output then set it.
+  if($$settings{all}){
+    $$settings{$_}=1 for(qw(confidence taxa));
+  }
 
   my @tree=@ARGV;
 
-  print join("\t",qw(File numLeaves numNodes))."\n";
+  my @header=qw(File numLeaves numNodes);
+  push(@header,'confidence') if($$settings{confidence});
+  push(@header,'taxa') if($$settings{taxa});
+  print join("\t",@header)."\n";
   for my $t(@tree){
     treeInfo($t,$settings);
   }
@@ -28,14 +37,57 @@ sub treeInfo{
 
   my $t=Bio::TreeIO->new(-file=>$tree)->next_tree;
   my @leaf=map{$_->id} $t->get_leaf_nodes;
-  my @node=map{$_->id} $t->get_nodes;
+  my @node=$t->get_nodes;
 
-  print join("\t",$tree,scalar(@leaf),scalar(@node))."\n";
+
+  my @out=($tree,scalar(@leaf),scalar(@node));
+  if($$settings{confidence}){
+    my $sum=0;
+    my $count=0;
+    my @bootstrap;
+    for my $n(@node){
+      next if($n->get_all_Descendents < 1);
+      my $bootstrap=$n->bootstrap || $n->id;;
+      next if(!defined($bootstrap));
+      next if($bootstrap < $$settings{cutoff});
+      push(@bootstrap,$bootstrap);
+      $sum+=$bootstrap;
+      $count++;
+    }
+    
+    # does median or avg make more sense here?
+    my $median=sprintf("%0.2f",median(@bootstrap));
+    my $avg=sprintf("%0.2f",$sum/$count);
+    push(@out,$avg);
+  }
+
+  push(@out,join(",",@leaf)) if($$settings{taxa});
+
+  print join("\t",@out)."\n";
 }
+
+sub median
+{
+    my @vals = sort {$a <=> $b} @_;
+    my $len = @vals;
+    if($len%2) #odd?
+    {
+        return $vals[int($len/2)];
+    }
+    else #even
+    {
+        return ($vals[int($len/2)-1] + $vals[int($len/2)])/2;
+    }
+}
+
 
 sub usage{
   $0=basename $0;
   "$0: gives information about a phylogeny
   Usage: $0 tree.dnd [tree2.dnd...]
+  --taxa         Shows taxon names
+  --confidence   Average confidence/bootstrap values
+  --cutoff     0 Do not consider confidence values below this value
+  --all          Shows all possible output fields
   "
 }
