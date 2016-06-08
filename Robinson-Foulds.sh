54a6e5eef24b8d1540ffe0f68afae6e0df67e3f2
Lee Katz - Aspen <gzu2@cdc.gov>
2016-06-08 09:03:36 -0400

    refactored; using Math::Gauss for better p values

--- Robinson-Foulds.sh
+++ Robinson-Foulds.sh
@@ -27,7 +27,8 @@ tmpdir=$(mktemp --directory --tmpdir=/tmp/$USER Robinson-Foulds.XXXXXX)
 if [ $? -gt 0 ]; then logmsg "ERROR making temporary directory under /tmp/$USER"; exit 1; fi;
 logmsg "Temporary dir is $tmpdir";
 
-cp $tree1 $tree2 $tmpdir/
+cp $tree1 $tmpdir/ &&\
+cp $tree2 $tmpdir/
 
 t1="$tmpdir/$(basename $tree1)"
 t2="$tmpdir/$(basename $tree2)"
@@ -38,19 +39,22 @@ t2="$tmpdir/$(basename $tree2)"
 #   the the tree to compare against's parameters
 comparisonTrees="$tmpdir/compareAgainst.dnd";
 cp $t2 $comparisonTrees
-randTrees.pl $t2 --numTrees 100 >> $comparisonTrees
+randTrees.pl $t1 --numTrees 100 >> $comparisonTrees
 ln -s $t1 "$tmpdir/intree"
 ln -s $comparisonTrees "$tmpdir/intree2"
 
 # Must do treedist in the temp directory because it pollutes the
 # local directory
 pushd $tmpdir > /dev/null 2>&1
-echo -ne 'D\n2\nL\nS\nY\nY\n' | treedist > "$tmpdir/treedist.log" 2> "$tmpdir/treedist.out"
+
+# Run treedist with the Kuhner and Felsenstein distance metric.
+# If the option "D" were given, then it would be run with Robinson-Foulds
+#echo -ne 'D\n2\nL\nS\nY\n' | treedist > "$tmpdir/treedist.log" 2> "$tmpdir/treedist.out"
+echo -ne '2\nL\nS\nY\n' | treedist > "$tmpdir/treedist.log" 2> "$tmpdir/treedist.out"
 if [ $? -gt 0 ]; then logmsg "ERROR with treedist program: $(cat $tmpdir/treedist.log)"; exit 1; fi;
 
 # Find average and stdev
-total=$(cut -f 3 -d ' ' "$tmpdir/outfile")
-cat outfile | perl -MList::Util=sum -lane '
+cat outfile | perl -MStatistics::Descriptive -MMath::Gauss=cdf,pdf -MList::Util=sum -lane '
   BEGIN{
     my @F=split(/\s+/,<>);
     $obs=$F[2];
@@ -58,24 +62,26 @@ cat outfile | perl -MList::Util=sum -lane '
   next if($F[0] != 1);
   push(@difference,$F[2]);
   END{
+    $stat=Statistics::Descriptive::Full->new();
+    $stat->add_data(@difference);
     $num=@difference;
-    $total=sum(@difference);
-    $avg=($total/$num);
-    
-    $SS=0;
-    for(@difference){
-      $SS = $SS + ($_-$avg)**2;
-    }
-    $stdev=sqrt($SS/($num-1));
+    $avg=$stat->mean;
+    $stdev=$stat->standard_deviation;
     $var=$stdev**2;
 
     $Z=($obs - $avg)/$stdev;
-    $p=exp(-($obs - $avg)**2/(2 * $var))/sqrt(8 * atan2(1,1) * $var);
+    $p=cdf($obs,$avg,$stdev);
+    #$p=cdf($Z);
 
-    $_=sprintf("%0.2f",$_) for($avg,$stdev,$Z,$p);
+    # scientific and floating point formatting
+    $_=sprintf("%0.2e",$_) for($p);
+    $_=sprintf("%0.2f",$_) for($obs,$avg,$stdev,$Z);
+
+    # Print results
     print join("\t",qw(obs num avg stdev Z p));
     print join("\t",$obs, $num, $avg,$stdev,$Z,$p);
   }
 '
 
 rm -rf $tmpdir
+
