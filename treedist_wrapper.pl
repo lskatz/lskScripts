51da0a15f8bbad768069e7a02d7580b289604ded
Lee Katz - Aspen <gzu2@cdc.gov>
2016-08-11 14:26:31 -0400

    added Bio::Phylo

--- treedist_wrapper.pl
+++ treedist_wrapper.pl
@@ -4,14 +4,27 @@ use strict;
 use warnings;
 use Data::Dumper;
 use File::Basename qw/basename/;
-use File::Temp qw/tempdir/;
+use File::Temp qw/tempdir tempfile/;
 use File::Copy qw/cp mv/;
 use File::Spec;
 use Getopt::Long;
 use Statistics::Descriptive;
 use Math::Gauss qw/cdf pdf/;
 
-sub logmsg{local $0=basename $0; print STDERR "$0 @_\n";}
+use Bio::Phylo::IO;
+use Bio::Phylo::Factory;
+use Bio::TreeIO;
+
+use threads;
+
+my $startTime=time();
+sub logmsg{
+  my $duration=time() - $startTime;
+  local $0=basename $0; 
+  my $TID='TID'.threads->tid;
+  #print STDERR "$0 [$duration]: @_\n";
+  print STDERR "$0($TID): @_\n";
+}
 
 exit main();
 
@@ -19,14 +32,18 @@ sub main{
   my $settings={};
   GetOptions($settings,qw(help tempdir=s method=s numcpus=i numtrees=i)) or die $!;
   $$settings{numcpus}||=1;
-  $$settings{numtrees}||=1000;
+  $$settings{numtrees}||=0;
   $$settings{method}||="kf";
+  $$settings{method}||=lc($$settings{method});
   $$settings{tempdir}||=tempdir(basename($0).".XXXXXX",TMPDIR=>1,CLEANUP=>1);
   $$settings{tempdir}=File::Spec->rel2abs($$settings{tempdir});
 
   my($ref,@query)=@ARGV;
 
   die usage() if(!$ref || !@query || $$settings{help});
+  for($ref,@query){
+    die "ERROR: cannot find $_" if(!-e $_);
+  }
 
   # Check executables
   for my $exe (qw(treedist randTrees.pl)){
@@ -38,17 +55,12 @@ sub main{
 
   print join("\t",qw(Ref Query num obs avg stdev Z p))."\n";
   for my $query(@query){
-    my $stat={};
-    $$settings{method}=lc($$settings{method});
-    if($$settings{method} eq "kf"){
-      $stat=kuhnerFelsenstein($ref,$query,$settings);
-    } elsif($$settings{method} eq "rf"){
-      $stat=robinsonFoulds($ref,$query,$settings);
-    } else {
-      die "ERROR: I do not understand method $$settings{method}";
-    }
+    my $stat=bioPhyloDist($ref,$query,$$settings{method},$settings);
 
-    $$stat{$_}=sprintf("%0.2f",$$stat{$_}) for(qw(obs avg stdev Z p));
+    # Round to two decimal places for most things
+    $$stat{$_}=sprintf("%0.4f",$$stat{$_}) for(qw(obs avg stdev Z));
+    # Scientific notation for p values
+    $$stat{$_}=sprintf("%0.2e",$$stat{$_}) for(qw(p));
     print join("\t",$$stat{ref},$$stat{query}, $$stat{num}, $$stat{obs},
                     $$stat{avg}, $$stat{stdev}, $$stat{Z}, $$stat{p});
     print "\n";
@@ -57,151 +69,137 @@ sub main{
   return 0;
 }
 
-sub kuhnerFelsenstein{
-  my($ref,$query,$settings)=@_;
+sub bioPhyloDist{
+  my($ref,$query,$method,$settings)=@_;
 
-  # Clean out certain files in the tempdir that may
-  # or may not be there.
-  # Unfortunately treedist has problems with overwriting
-  # files.
-  for("$$settings{tempdir}/intree","$$settings{tempdir}/intree2","$$settings{tempdir}/outfile"){
-    unlink($_);
-  }
-  
-  cp($ref,$$settings{tempdir}) or die $!;
-  cp($query,$$settings{tempdir}) or die $!;
+  # First find the observed value
+  my $obs=observedScore($query,$ref,$$settings{method},$settings);
 
-  my $tmpRefTree="$$settings{tempdir}/".basename($ref);
-  my $tmpQueryTree="$$settings{tempdir}/".basename($query);
+  # Next run a test against a random background distribution
 
-  
-  # Create a list of trees to compare against in $comparisonTrees:
-  #   The first tree is the reference tree
-  #   The next trees are randomly made trees from 
-  #   the query tree.
-  #   Therefore, the question being answered is, is the query
-  #   closer to the ref than random trees?
-  my $comparisonTrees="$$settings{tempdir}/compareAgainst.dnd";
-  cp($tmpRefTree,$comparisonTrees) or die $!;
-  logmsg "Generating $$settings{numtrees} random trees";
-  system("randTrees.pl $tmpQueryTree --force-binary --numcpus $$settings{numcpus} --numtrees $$settings{numtrees} >> $comparisonTrees"); die if $?;
-  # We need the files to be named intree and intree2 because of inflexible treedist
-  symlink(basename($tmpQueryTree),"$$settings{tempdir}/intree");
-  symlink(basename($comparisonTrees),"$$settings{tempdir}/intree2");
+  my (@scores); # scores from the statistical test
+  my ($avg,$stdev,$Z,$p)=(0) x 4;
 
-
-  system("cd $$settings{tempdir} && echo -ne '2\nL\nS\nY\n' | treedist > treedist.log 2> treedist.out");
-  die "ERROR with treedist ".`cat $$settings{tempdir}/treedist.log` if $?;
-
-  # Read the output file.
-  # "1" in the outfile indicates the query tree.
-  # "2" in the outfile indicates the ref tree.
-  # >2 indicates a random tree.
-  # I only care about query vs ref and query vs randoms.
-  # I do not care about anything vs query.
-  my @score;  # background distribution
-  my $obs;    # observed value
-  open(TREEDISTOUT,"$$settings{tempdir}/outfile") or die "ERROR: could not read treedist output: $!";
-  while(<TREEDISTOUT>){
-    chomp;
-    my($tree1,$tree2,$score)=split(/\s+/,$_);
-    next if($tree1 ne 1);
-    
-    if($tree2 eq 2){
-      $obs=$score;
-      next;
+  # Need more than 1 for a statistical test against
+  # a distribution
+  if($$settings{numtrees} > 1){
+    # multithreaded random trees
+    my @thr;
+    my $treesPerThread=int($$settings{numtrees}/$$settings{numcpus})+1;
+    for(0..$$settings{numcpus}-1){
+      $thr[$_]=threads->new(\&randScores,$query,$ref,$$settings{method},$treesPerThread,$settings);
     }
-    push(@score,$score);
-  }
-  close TREEDISTOUT;
-  
-  my $stat=Statistics::Descriptive::Full->new();
-  $stat->add_data(@score);
-  my $avg=$stat->mean;
-  my $stdev=$stat->standard_deviation;
-  
-  my $Z=($obs - $avg)/$stdev;
-  my $p=cdf($obs,$avg,$stdev);
 
-  return {Z=>$Z, p=>$p, obs=>$obs, num=>scalar(@score), avg=>$avg, stdev=>$stdev, query=>$query, ref=>$ref};
+    # Combine all random query trees. Results are found
+    # in $randTreesQ
+    for(@thr){
+      my $scoresList=$_->join;
+      # There was a problem with $scoresList and I put in this
+      # debugging statement, but there really isn't a 
+      # reason to remove it and so I kept it.
+      die "INTERNAL ERROR: scoresList is not a list!" if(ref($scoresList) ne "ARRAY");
+      push(@scores,@$scoresList);
+    }
+    # Respect the number of trees requested
+    splice(@scores,$$settings{numtrees});
+
+    my $stat=Statistics::Descriptive::Full->new();
+    $stat->add_data(@scores);
+    my $avg=$stat->mean;
+    my $stdev=$stat->standard_deviation;
+    my $Z=($obs - $avg)/$stdev;
+    my $p=cdf($obs,$avg,$stdev);
+  }
+
+  
+  return {Z=>$Z, p=>$p, obs=>$obs, num=>scalar(@scores), avg=>$avg, stdev=>$stdev, query=>$query, ref=>$ref};
+
 }
 
-sub robinsonFoulds{
-  my($ref,$query,$settings)=@_;
+sub randScores{
+  my($baseTree,$ref,$method,$numTrees,$settings)=@_;
 
-  # Clean out certain files in the tempdir that may
-  # or may not be there.
-  # Unfortunately treedist has problems with overwriting
-  # files.
-  for("$$settings{tempdir}/intree","$$settings{tempdir}/intree2","$$settings{tempdir}/outfile"){
-    unlink($_);
+  my $refObj=Bio::Phylo::IO->parse(
+    -format=>"newick",
+    -file=>$ref,
+  )->first;
+
+  logmsg "Making random trees from $baseTree";
+  my @newickString=`randTrees.pl $baseTree --force-binary --numcpus 1 --numtrees $numTrees 2>/dev/null`;
+  die "ERROR with randTrees.pl: $!" if $?;
+
+  logmsg "Comparing random trees against $ref";
+  my @dist;
+  for my $newick(@newickString){
+    my ($fh, $randQueryTree) = tempfile("randQuery.XXXXXX",DIR=>$$settings{tempdir},SUFFIX=>".dnd");
+    print $fh $newick;
+    close $fh;
+    push(@dist,observedScore($randQueryTree,$ref,$method,$settings));
   }
-  
-  cp($ref,$$settings{tempdir}) or die $!;
-  cp($query,$$settings{tempdir}) or die $!;
 
-  my $tmpRefTree="$$settings{tempdir}/".basename($ref);
-  my $tmpQueryTree="$$settings{tempdir}/".basename($query);
-
-  
-  # Create a list of trees to compare against in $comparisonTrees:
-  #   The first tree is the reference tree
-  #   The next trees are randomly made trees from 
-  #   the query tree.
-  #   Therefore, the question being answered is, is the query
-  #   closer to the ref than random trees?
-  my $comparisonTrees="$$settings{tempdir}/compareAgainst.dnd";
-  cp($tmpRefTree,$comparisonTrees) or die $!;
-  logmsg "Generating $$settings{numtrees} random trees";
-  system("randTrees.pl $tmpQueryTree --numcpus $$settings{numcpus} --numtrees $$settings{numtrees} >> $comparisonTrees"); die if $?;
-  # We need the files to be named intree and intree2 because of inflexible treedist
-  symlink(basename($tmpQueryTree),"$$settings{tempdir}/intree");
-  symlink(basename($comparisonTrees),"$$settings{tempdir}/intree2");
-
-
-  system("cd $$settings{tempdir} && echo -ne 'D\n2\nL\nS\nY\n' | treedist > treedist.log 2> treedist.out");
-  die "ERROR with treedist ".`cat $$settings{tempdir}/treedist.log` if $?;
-
-  # Read the output file.
-  # "1" in the outfile indicates the query tree.
-  # "2" in the outfile indicates the ref tree.
-  # >2 indicates a random tree.
-  # I only care about query vs ref and query vs randoms.
-  # I do not care about anything vs query.
-  my @score;  # background distribution
-  my $obs;    # observed value
-  open(TREEDISTOUT,"$$settings{tempdir}/outfile") or die "ERROR: could not read treedist output: $!";
-  while(<TREEDISTOUT>){
-    chomp;
-    my($tree1,$tree2,$score)=split(/\s+/,$_);
-    next if($tree1 ne 1);
-    
-    if($tree2 eq 2){
-      $obs=$score;
-      next;
-    }
-    push(@score,$score);
-  }
-  close TREEDISTOUT;
-  
-  my $stat=Statistics::Descriptive::Full->new();
-  $stat->add_data(@score);
-  my $avg=$stat->mean;
-  my $stdev=$stat->standard_deviation;
-  
-  my $Z=($obs - $avg)/$stdev;
-  my $p=cdf($obs,$avg,$stdev);
-
-  return {Z=>$Z, p=>$p, obs=>$obs, num=>scalar(@score), avg=>$avg, stdev=>$stdev, query=>$query, ref=>$ref};
+  return \@dist;
 }
+
+sub observedScore{
+  my($query,$ref,$method,$settings)=@_;
+
+  my $queryObj=Bio::Phylo::IO->parse(
+    -format=>"newick",
+    -file=>$query,
+  )->first;
+  my $refObj=Bio::Phylo::IO->parse(
+    -format=>"newick",
+    -file=>$ref,
+  )->first;
+
+  for($queryObj,$refObj){
+    # Break polytomies and make the tree binary.
+    $_=$_->resolve()->deroot();
+
+    # midpoint root: makes the score not match Phylip
+    #my $midpointNode=$_->get_midpoint();
+    #$_->reroot($midpointNode);
+    #$_=$_->deroot();
+  }
+
+  #logmsg "$refObj $queryObj";
+  #if(!$refObj->is_binary()){
+  #  die "ERROR: the ref is not binary!\n  file: $ref\n  ".`cat $ref`;
+  #}
+  #if(!$queryObj->is_binary()){
+  #  die "ERROR: the query is not binary!\n  file: $query\n  ".`cat $query`;
+  #}
+
+  # Find the score based on the method
+  my $obs;
+  if($method =~ /^kf(2)?$/){
+    $obs=$refObj->calc_branch_length_distance($queryObj,0);
+    # Branch length score is the branch length distance squared
+    if(defined($1)){
+      $obs=$obs**2;
+    }
+  } elsif($method eq 'rf'){
+    $obs=$refObj->calc_symdiff($queryObj);
+    $obs=$obs*2; # this version of RF always returns a single-sided value
+  } else {
+    die "ERROR: method $method is not implemented";
+  }
+
+  die "INTERNAL ERROR: invalid distance score" if(!defined($obs));
+
+  return $obs;
+}
+
 
 sub usage{
   local $0=basename $0;
   "$0: compares two trees
   Usage: $0 ref.dnd query.dnd
-  --method    kf     kf: kuhner-felsenstein
-                     rf: robinson-foulds
-  --numtrees  1000   How many random trees to compare against
+  --method    kf     kf:  kuhner-felsenstein (branch length distance)
+                     kf2: branch length score (k-f squared)
+                     rf:  robinson-foulds (symmetric distance)
+  --numtrees  1000   How many random trees to compare against?
+                     Use 0 to not run a statistical test.
   "
 }
 
