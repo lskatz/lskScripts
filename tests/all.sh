5743d0382325ea253e236d5d4d7bdf41631e1429
Lee Katz <lskatz@gmail.com>
2018-03-08 13:41:47 -0500

    in pursuit of exit codes

--- tests/all.sh
+++ tests/all.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
 set -e
-set -x
 
 # Find all unit tests under this directory
 # and simply run them with -e and -x
-find $(dirname $0)/unittests -maxdepth 1 -type f | \
-  while read exe; do
-    $exe
-    if [ $? -gt 0 ]; then
-      exit 1
-    fi
-  done;
+executables=$(find $(dirname $0)/unittests -maxdepth 1 -type f)
+for exe in $executables; do
+  $exe
+  if [ $? -gt 0 ]; then
+    exit 1
+  fi
+done;
 
 exit 0
