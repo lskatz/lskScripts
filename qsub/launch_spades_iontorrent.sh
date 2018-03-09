2e61902f4d62670ef4df31b810ff5b29a29250d5
Lee Katz - Aspen <gzu2@cdc.gov>
2018-03-09 10:15:53 -0500

    more launch commands

---
+++ qsub/launch_spades_iontorrent.sh
@@ -0,0 +1,32 @@
+#!/bin/bash -l
+#$ -S /bin/bash
+#$ -pe smp 4-16
+#$ -cwd -V
+#$ -o spades.log
+#$ -j y
+#$ -N SPAdesIonTorrent3.6.2
+#$ -q all.q
+
+reads=$1
+out=$2
+fastaOut=$3
+
+if [ "$out" == "" ]; then
+  echo "Usage: $0 reads.sff output/ [out.fasta]"
+  echo "  if out.fasta is given, then the output directory will be removed and the scaffolds.fasta file will be saved"
+  exit 1;
+fi;
+
+module load SPAdes/3.6.2
+if [ $? -gt 0 ]; then echo "unable to load spades 3.6.2"; exit 1; fi;
+
+NSLOTS=${NSLOTS:=1}
+spades.py -s $reads --iontorrent -o $out -t $NSLOTS 
+if [ $? -gt 0 ]; then echo "problem with spades 3.6.2"; exit 1; fi;
+
+if [ "$fastaOut" != "" ]; then
+  cp -v "$out/scaffolds.fasta" $fastaOut
+  if [ $? -gt 0 ]; then echo "problem with copying $out/scaffolds.fasta => $fastaOut"; exit 1; fi;
+  rm -rf "$out";
+  if [ $? -gt 0 ]; then echo "problem with removing the directory $out"; exit 1; fi;
+fi
