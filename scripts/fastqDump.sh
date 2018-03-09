a2a9b9850834dbbe04fc302767d15cf29c1f2973
Lee Katz - Aspen <gzu2@cdc.gov>
2018-03-09 10:17:42 -0500

    smarter tempdir

--- scripts/fastqDump.sh
+++ scripts/fastqDump.sh
@@ -9,6 +9,15 @@ if [ "$SRR" == "" ]; then
   exit 1;
 fi
 
-fastq-dump --accession $SRR --outdir . --defline-seq '@$ac.$si/$ri' --defline-qual '+' --split-files --skip-technical --dumpbase --clip
+TEMPDIR=$(mktemp --directory $(basename $0).XXXXXX --tmpdir)
+trap "{ rm -rf $TEMPDIR; }" EXIT SIGINT SIGTERM
+echo "Files will temporarily be stored in $TEMPDIR"
 
-exit $? # carry over the exit code
+fastq-dump --gzip --accession $SRR --outdir $TEMPDIR --defline-seq '@$ac.$si/$ri' --defline-qual '+' --split-files --skip-technical --dumpbase --clip
+
+if [ $? -gt 0 ]; then 
+  echo "ERROR with fastq-dump and $SRR"
+  exit 1
+fi
+
+mv $TEMPDIR/* .
