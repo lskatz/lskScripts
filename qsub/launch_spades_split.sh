60bbf9c586daabae28105ce33718a8db6de007c8
Lee Katz <lskatz@gmail.com>
2018-04-05 15:54:43 -0400

    v3.11

--- qsub/launch_spades_split.sh
+++ qsub/launch_spades_split.sh
@@ -1,30 +1,39 @@
 #!/bin/bash -l
+# Runs a spades assembly.  Run with no options for usage.
+# Author: Lee Katz <lkatz@cdc.gov>
+#Example:  (for f in *R1_001.fastq.gz; do b=`basename $f _R1.fastq.gz`; r=`sed 's/R1/R2/' <<< $f`; qsub -N spades$b -o ./assemblies/log/b.spades.log ~/bin/launch_SPAdes_v3.11.0.sh $f $r ./assemblies/$b.spades3.11 ./assemblies/$b; done;)
+
 #$ -S /bin/bash
 #$ -pe smp 4-16
 #$ -cwd -V
 #$ -o spades.log
 #$ -j y
-#$ -N SPAdes3.9.0
+#$ -N SPAdes3.11.0
 #$ -q all.q
 
-fwd=$1
-rev=$2
+forward=$1
+reverse=$2
 out=$3
 fastaOut=$4
 
+source /etc/profile.d/modules.sh
+scriptname=$(basename $0);
+
 if [ "$out" == "" ]; then
-  echo "Usage: $0 fwd.fastq.gz rev.fastq.gz output/ [out.fasta]"
+  echo "Usage: $scriptname reads.1.fastq.gz reads.2.fastq.gz output/ [out.fasta]"
   echo "  if out.fasta is given, then the output directory will be removed and the scaffolds.fasta file will be saved"
   exit 1;
 fi;
 
-module load SPAdes/3.9.0
-if [ $? -gt 0 ]; then echo "unable to load spades 3.9.0"; exit 1; fi;
-module load Python/2.7.12
+module load SPAdes/3.11.0
+if [ $? -gt 0 ]; then echo "unable to load spades 3.11.0"; exit 1; fi;
 
 NSLOTS=${NSLOTS:=1}
-spades.py -1 $fwd -2 $rev --careful -o $out -t $NSLOTS 
-if [ $? -gt 0 ]; then echo "problem with spades 3.9.0"; exit 1; fi;
+
+COMMAND="spades.py -1 $forward -2 $reverse --careful -o $out -t $NSLOTS"
+echo "$scriptname: $COMMAND"
+$COMMAND
+if [ $? -gt 0 ]; then echo "problem with spades 3.11.0"; exit 1; fi;
 
 if [ "$fastaOut" != "" ]; then
   cp -v "$out/scaffolds.fasta" $fastaOut
