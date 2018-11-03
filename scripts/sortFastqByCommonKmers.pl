52cfd21a01c20e09d570a1f618d67e1a425be3dc
Lee Katz <lskatz@gmail.com>
2018-11-03 14:31:28 -0400

    new fastq sorting script

---
+++ scripts/sortFastqByCommonKmers.pl
@@ -0,0 +1,112 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+use Data::Dumper;
+use File::Basename qw/basename/;
+use Getopt::Long qw/GetOptions/;
+use List::MoreUtils qw/uniq/;
+
+exit main();
+
+sub main{
+  my $settings={};
+  GetOptions($settings,qw(help kmerlength=s)) or die $!;
+  $$settings{kmerlength}||="31,21,11";
+
+  my @kmerlength=split(/,/,$$settings{kmerlength});
+  for(my $i=0;$i<@ARGV;$i++){
+    my $R1=$ARGV[$i];
+    my $R2=$ARGV[$i+1];
+    
+    my $kmerInfo      = countKmers ($R1,$R2,\@kmerlength,$settings);
+    my $sortedEntries = sortEntries($kmerInfo,$settings);
+  }
+
+  return 0;
+}
+
+sub sortEntries{
+  my($kmerInfo, $settings)=@_;
+  my $entries     = $$kmerInfo{read};
+  my $kmerCounter = $$kmerInfo{kmerCounter};
+  my $kmer_to_read= $$kmerInfo{kmer_to_read};
+
+  my @kmerLength = sort {$a <=> $b} keys(%$kmerCounter);
+
+  # Find the most common kmer of each length
+  my %topKmer;
+  for my $kmerLength(@kmerLength){
+    my $maxcount=0;
+    my $topKmer ="";
+    while(my($kmer, $count) = each(%{$$kmerCounter{$kmerLength}})){
+      if($count > $maxcount){
+        $topKmer  = $kmer." => $count";
+        $maxcount = $count;
+      }
+    }
+    $topKmer{$kmerLength} = $topKmer;
+  }
+
+  # For each kmer length, print out resulting reads
+  for my $kmerLength(@kmerLength){
+    my @readId = @{ $$kmer_to_read{$topKmer{$kmerLength}} };
+    die Dumper \@readId;
+
+    # print the reads
+    # remove from the entries hash
+  }
+}
+
+sub countKmers{
+  my($R1,$R2,$kmerlength,$settings)=@_;
+  my @kmerlength=@$kmerlength; # bring into scope to make it faster(?)
+
+  # read{id} => 8-line-str, 
+  my %read;
+  # kmerCounter => {31 =>{AAA=>4,...}, 21=>{...}
+  my %kmerCounter;
+  # kmer_to_read => {AAA => id1, ...}
+  my %kmer_to_read;
+
+  open(my $fh1,"zcat $R1|") or die "ERROR: could not open $R1: $!";
+  open(my $fh2,"zcat $R2|") or die "ERROR: could not open $R2: $!";
+  while(my $id1 = <$fh1>){
+    my $seq1  = <$fh1>;
+    my $plus1 = <$fh1>;
+    my $qual1 = <$fh1>;
+    my $id2   = <$fh2>;
+    my $seq2  = <$fh2>;
+    my $plus2 = <$fh2>;
+    my $qual2 = <$fh2>;
+    my $entry = "$id1$seq1,$plus1,$qual1,$id2,$seq2,$plus2,$qual2";
+    chomp($id1,$seq1,$plus1,$qual1,$id2,$seq2,$plus2,$qual2);
+
+    # Save the read
+    $read{$id1} = $entry;
+
+    # kmerizing
+    for my $k(@kmerlength){
+      my $seqkLength = length($seq1) - $k + 1;
+      
+      # kmers for each window
+      for(my $i=0;$i<$seqkLength;$i++){
+        my $kmer = substr($seq1, $i, $k);
+
+        # count the kmer
+        $kmerCounter{$k}{$kmer}++;
+
+        # Mark that this kmer is in this read
+        #$kmer_to_read{$kmer}{$id1}=1;
+        push(@{$kmer_to_read{$kmer}}, $id1);
+        #$kmer_to_read{$kmer} = [uniq(@{$kmer_to_read{$kmer}})];
+      }
+    }
+  }
+  close $fh2;
+  close $fh1;
+  
+  return {read=>\%read,kmerCounter=>\%kmerCounter,kmer_to_read=>\%kmer_to_read};
+}
+
+
