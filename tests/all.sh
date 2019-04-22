9aabeaec13b139caf090986a3b4e6d4558db7a47
Lee Katz - Aspen <gzu2@cdc.gov>
2019-04-22 09:55:38 -0400

    export PATH to scripts

--- tests/all.sh
+++ tests/all.sh
@@ -2,6 +2,9 @@
 
 set -e
 
+scriptDir=$(dirname $0);
+export PATH=$PATH:$scriptDir/../scripts
+
 # Find all unit tests under this directory
 # and simply run them with -e and -x
 executables=$(find $(dirname $0)/unittests -maxdepth 1 -type f -name '*.sh')
