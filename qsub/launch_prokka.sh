440816a80a0d8502fe5032a8b59005052127b5e9
Lee Katz - Aspen <gzu2@cdc.gov>
2021-01-05 20:39:34 -0500

    load modules after usage

--- qsub/launch_prokka.sh
+++ qsub/launch_prokka.sh
@@ -9,7 +9,6 @@ contigs=$1
 genome=$2
 
 source /etc/profile.d/modules.sh
-module load prokka/1.13.3
 
 genus=${3-genus}
 species=${4-species}
@@ -20,6 +19,8 @@ if [ "$genome" == "" ]; then
   echo "Usage: $script contigs.fasta genomename [genus species]"
   exit 1;
 fi
+module load prokka/1.13.3
+module load rnammer/1.2
 
 command="prokka --prefix $genome --compliant --locustag $genome --genus $genus --species $species --strain $genome --force --cpus $NSLOTS $contigs"
 eval $command
