c1bbed083f68ff82b03ff0d8977661e57e9fa6ce
Lee Katz - Aspen <gzu2@cdc.gov>
2022-05-09 13:59:45 -0400

    updated sorting/compression

--- scripts/fastqDump.sh
+++ scripts/fastqDump.sh
@@ -3,14 +3,20 @@
 SRR=$1
 
 R1="${SRR}_1.fastq.gz";
+R2="${SRR}_2.fastq.gz";
 R1uncompressed="${SRR}_1.fastq"
+R2uncompressed="${SRR}_2.fastq"
 
 script=$(basename $0);
 if [ "$SRR" == "" ]; then
   echo "Downloads a fastq properly using fastq-dump"
+  echo "  Sorts the reads for maximum compression using fasten_sort."
   echo "Usage: $script SRR_accession"
   exit 1;
 fi
+
+set -e
+set -u
 
 if [ -e "${SRR}.fastq.gz" ]; then
   echo "${SRR}.fastq.gz is already present."
@@ -24,24 +30,26 @@ fi
 module purge
 module load sratoolkit/2.9.1
 
+# Check if fasten_sort is in the path and if not, quit
+echo "Checking dependency paths"
+which fasten_sort
+which fasterq-dump || which fastq-dump
+
 tempdir=$(mktemp --directory --tmpdir=$TMPDIR $(basename $0).XXXXXX)
 trap "{ rm -rf $tempdir; }" EXIT SIGINT SIGTERM
 echo "Files will temporarily be stored in $tempdir"
 
 # Decide whether to run fastq-dump or fasterq-dump
-if [ "$(which fasterq-dump 2>/dev/null)" == "" ]; then
-  set -x
-  fastq-dump --gzip --accession $SRR --outdir $tempdir --defline-seq '@$ac.$si/$ri' --defline-qual '+' --split-files --skip-technical --dumpbase --clip
-  set +x
+fasterqDump="$(which fasterq-dump 2>/dev/null)";
+if [ "$fasterqDump" == "" ]; then
+  fastq-dump --accession $SRR --outdir $tempdir --defline-seq '@$ac.$si/$ri' --defline-qual '+' --split-files --skip-technical --dumpbase --clip
   if [ $? -gt 0 ]; then 
     echo "ERROR with fastq-dump and $SRR"
     exit 1
   fi
 else
-  set -x
   cd $tempdir
   fasterq-dump $SRR --print-read-nr --threads 1 --outdir $tempdir --split-files --skip-technical 
-  set +x
   if [ $? -gt 0 ]; then 
     echo "ERROR with fasterq-dump and $SRR"
     exit 1
@@ -63,11 +71,23 @@ else
       print;
     ' < $fastq > $fastq.tmp;
     mv $fastq.tmp $fastq
-    gzip $fastq &
   done;
-  wait;
 fi
 
+# Intense compression
+#echo "DEBUG"; head -n 888 $tempdir/$R1uncompressed > $tempdir/unsorted_1.fastq
+#echo "DEBUG"; head -n 888 $tempdir/$R2uncompressed > $tempdir/unsorted_2.fastq
+mv -v $tempdir/$R1uncompressed $tempdir/unsorted_1.fastq
+mv -v $tempdir/$R2uncompressed $tempdir/unsorted_2.fastq
+cat $tempdir/unsorted_1.fastq $tempdir/unsorted_2.fastq | \
+  fasten_shuffle | \
+  fasten_sort --sort-by SEQ --paired-end | \
+  fasten_progress --print --id sort-reads --update-every 100000 | \
+  fasten_shuffle -d -1 $tempdir/$R1uncompressed -2 $tempdir/$R2uncompressed
+gzip -v9 $tempdir/$R1uncompressed $tempdir/$R2uncompressed
+rm -v $tempdir/unsorted_1.fastq $tempdir/unsorted_2.fastq
 
-mv -v $tempdir/* .
+ls -lhd $tempdir
+ls -lh $tempdir/*
+mv -v $tempdir/{$R1,$R2} .
 
