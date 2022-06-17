fb9499aa56d8b5eecb3323630cb4cae9573ed7af
Lee Katz - Aspen <gzu2@cdc.gov>
2022-06-17 14:52:45 -0400

    reformatting a bit

--- scripts/qsubStats.sh
+++ scripts/qsubStats.sh
@@ -16,4 +16,8 @@ QSTAT=$(echo "$QSTAT"|tail -n +3) # qstat, minus the header
 # echo "$QSTAT" |tail -n +3| perl -MData::Dumper -e 'while(<>){s/^\s+|\s+$//g; @F=split /\s+/; next if($F[4] ne 'r'); $slots=$F[8]; if($F[3] eq $ENV{USER}){$mine+=$slots;} $total+=$slots; } print "$mine out of $total\n";'
 
 # who is the current hog
-echo "$QSTAT" | perl -lane 'BEGIN{print "USER\tSLOTS";} next if(!$F[3] || !$F[8]); next if($F[4] ne "r"); $slot{$F[3]}+=$F[8]; END{@user=sort{$slot{$b}<=>$slot{$a} || $a cmp $b} keys(%slot); print "$_\t$slot{$_}" for @user;}' | column -t
+echo "$QSTAT" | perl -lane '
+  BEGIN{print "USER\tSLOTS";} 
+  next if(!$F[3] || !$F[8]); 
+  next if($F[4] !~ /^R?r$/); 
+  $slot{$F[3]}+=$F[8]; END{@user=sort{$slot{$b}<=>$slot{$a} || $a cmp $b} keys(%slot); print "$_\t$slot{$_}" for @user;}' | column -t
