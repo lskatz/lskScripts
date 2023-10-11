1bcdb270841df255aa2a5b1da3805826636f8268
Lee Katz <gzu2@cdc.gov>
2023-10-11 14:42:57 -0400

    Update launch_shovill.sh

--- qsub/launch_shovill.sh
+++ qsub/launch_shovill.sh
@@ -26,11 +26,9 @@ fi;
 set -e
 set -u
 
-export PATH=$PATH:$HOME/bin/shovill-1.0.9/bin
-module load SPAdes/3.13.0 Skesa/2.3.0 megahit/1.1.2 velvet/1.2.10 lighter/1.1.1 flash/1.2.11 samtools/1.9 bwa/0.7.17 Mash/2.0 seqtk/1.3 pilon/1.22 trimmomatic/0.35 perl/5.16.1-MT
+module load shovill
 
 shovill --check
 
-#shovill --outdir $outdir --R1 $R1 --R2 $R2
 shovill --outdir $outdir --R1 $R1 --R2 $R2 --assembler skesa
 
